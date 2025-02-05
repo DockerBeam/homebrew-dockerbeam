bd6b719c6e416dfaf04c462efa9e0bf6bddf6928
cryptodarth <89084149+Crypto-Darth@users.noreply.github.com>
2025-02-05 21:47:45 +0530

    Create homebrew formula

---
+++ dockerbeam.rb
@@ -0,0 +1,14 @@
+class dockerbeam < Formula
+  desc "A P2P Docker image sharing tool"
+  homepage "https://github.com/DockerBeam/DockerBeam"
+  url "https://github.com/DockerBeam/DockerBeam/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "6c71e571d86e689399b35bb9f0d449be6f82eb73a2503238f54146c2b968887a"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release", "--bin", "dockerbeam"
+    bin.install "target/release/dockerbeam"
+  end
+end
