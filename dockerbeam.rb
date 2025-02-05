22bb99a078445263e286110390674bbf97ff2f52
cryptodarth <89084149+Crypto-Darth@users.noreply.github.com>
2025-02-05 21:51:43 +0530

    Update dockerbeam.rb

--- dockerbeam.rb
+++ dockerbeam.rb
@@ -1,4 +1,4 @@
-class dockerbeam < Formula
+class DockerBeam < Formula
   desc "A P2P Docker image sharing tool"
   homepage "https://github.com/DockerBeam/DockerBeam"
   url "https://github.com/DockerBeam/DockerBeam/archive/refs/tags/v0.1.0.tar.gz"
