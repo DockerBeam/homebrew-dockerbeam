d1df2eac6ed5b85b706668d5cd54c04e6c9e0868
cryptodarth <89084149+Crypto-Darth@users.noreply.github.com>
2025-02-08 00:44:06 +0530

    Update dockerbeam.rb

--- dockerbeam.rb
+++ dockerbeam.rb
@@ -1,14 +1,11 @@
 class Dockerbeam < Formula
   desc "A P2P Docker image sharing tool"
   homepage "https://github.com/DockerBeam/DockerBeam"
-  url "https://github.com/DockerBeam/DockerBeam/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "6c71e571d86e689399b35bb9f0d449be6f82eb73a2503238f54146c2b968887a"
+  url "https://github.com/DockerBeam/DockerBeam/releases/download/v0.1.0/dockerbeam_macos-universal"
+  sha256 "0355032f243a7546c7f58c0e0193722bec939b595cbe309dfceb15a13a13a72b"
   license "MIT"
 
-  depends_on "rust" => :build
-
   def install
-    system "cargo", "build", "--release", "--bin", "DockerBeam"
-    bin.install "target/release/DockerBeam"
+    bin.install "dockerbeam_macos-universal" => "dockerbeam"
   end
 end
